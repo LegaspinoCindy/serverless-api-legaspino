aafd741d1c1ae6fa92d91ac0b3ed33ceb30e2a7f
LegaspinoCindy <cindylegaspino2@gmail.com>
2024-04-24 15:52:15 +0800

    2nd commit

--- functions/api.js
+++ functions/api.js
@@ -8,7 +8,7 @@ const app = express();
 //your mongoDB Cloud URL
 const dbCloudUrl = 'mongodb+srv://legaspinocindy:philippians4:13@cluster0.2mdm9bw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 //youy mongoDB Cloud URL
-const dbLocalUrl = 'mongodb://localhost:27017/serverless-api-legaspino';
+const dbLocalUrl = 'mongodb://localhost:27017/express-mongo-api';
 
 app.use(cors());
 app.use(express.json());
@@ -19,5 +19,5 @@ mongoose
 .then(() => console.log('Connected to MongoDB'))
 .catch((error) => console.error('Failed to connect to MOngoDB', error));
 
-app.use('/.netlify/functions/api', router);
+app.use('/.netlify/functions/api', authorRouter);
 module.exports.handler = serverless(app);
